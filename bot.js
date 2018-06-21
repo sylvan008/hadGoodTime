91f20216213e6c9341293a0a42aeb8873fad965a
Oleg Selin <sylvan.climb@gmail.com>
2018-06-21 17:32:39 +0300

    [bot.js] added start and message handlers

--- bot.js
+++ bot.js
@@ -1,8 +1,48 @@
 const Bot = require('node-telegram-bot-api');
+const db = require('./levelDb');
+const state = require('./state');
 
 const token = process.env.BOT_ACCESS_TOKEN;
 
 let bot = new Bot(token, { polling: true });
+
+let coffer = {
+  state: 0,
+  name: '',
+  expenses: '',
+  clients: []
+};
+
+const startHandler = chatId => {
+  coffer.state = state.ENTER_NAME;
+  db.putCoffer(chatId, coffer);
+  bot.sendMessage(chatId, "Давайте посчитаем вклад каждого в общий котёл. Введите имя.");
+};
+
+const messageHandler = async (chatId, text) => {
+  coffer = await db.getCoffer(chatId);
+
+  if (coffer.state === state.ENTER_NAME) {
+    coffer.name = text;
+    coffer.state = state.ENTER_EXPENSES;
+    bot.sendMessage(chatId, "Отлично! Сколько внёс " + coffer.name + "?");
+    await db.putCoffer(chatId, coffer);
+    return;
+  }
+
+  if (coffer.state === state.ENTER_EXPENSES) {
+    if (isNaN(text) || text < 0) {
+      bot.sendMessage(chatId, "Число некорректно или меньше нуля.");
+      return;
+    }
+    coffer.expenses = text;
+    await bot.sendMessage(chatId, "Так и запишем: " + coffer.name + " внёс " + coffer.expenses);
+    coffer.clients.push({name: coffer.name, expenses: coffer.expenses});
+    coffer.state = state.ENTER_NAME;
+    await db.putCoffer(chatId, coffer);
+    await bot.sendMessage(chatId, "Кто далее по списку?");
+  }
+};
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
@@ -24,9 +64,13 @@ bot.on('message', (msg) => {
      */
     const args = text.split(" ");
 
+    const chatId = msg.chat.id;
+
     if (args[0] === '/start') {
-      const chatId = msg.chat.id;
-      bot.sendMessage(chatId, "Hello World!");
+      startHandler(chatId);
+    }
+    else {
+      messageHandler(chatId, text);
     }
   }
 });
