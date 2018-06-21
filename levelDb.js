58abf661d00dfe1bcafa809dd81e0574f36063a7
Oleg Selin <sylvan.climb@gmail.com>
2018-06-21 17:09:23 +0300

    [levelDb.js] using async

--- levelDb.js
+++ levelDb.js
@@ -4,19 +4,12 @@ const db = level('./coffersDb', {
   valueEncoding: 'json'
 });
 
-db.getCoffer = chatId => {
-  return db.get(chatId, (err, coffer) => {
-    if (err && !err.notFound) throw err;
-    if (err && err.notFound) return console.log('Value was not found.');
-
-    return coffer;
-  });
+db.getCoffer = async chatId => {
+  return await db.get(chatId);
 };
 
-db.putCoffer = (chatId, coffer) => {
-  db.put(chatId, coffer, err => {
-    if (err) throw err;
-  });
+db.putCoffer = async (chatId, coffer) => {
+  return await db.put(chatId, coffer);
 };
 
 module.exports = db;
