0dddb4cc5d18cf447379eab06f66727376e1e2ab
Oleg Selin <sylvan.climb@gmail.com>
2018-06-21 13:28:20 +0300

    [levelDb.js] Added methods 'getCoffer' and 'putCoffer'

--- levelDb.js
+++ levelDb.js
@@ -1,7 +1,22 @@
 const level = require('level');
 
-const db = level('./mydb', {
+const db = level('./coffersDb', {
   valueEncoding: 'json'
 });
+
+db.getCoffer = chatId => {
+  return db.get(chatId, (err, coffer) => {
+    if (err && !err.notFound) throw err;
+    if (err && err.notFound) return console.log('Value was not found.');
+
+    return coffer;
+  });
+};
+
+db.putCoffer = (chatId, coffer) => {
+  db.put(chatId, coffer, err => {
+    if (err) throw err;
+  });
+};
 
 module.exports = db;
