331aa6d9a7555c2b6ffff9a1021234982ffe913a
Oleg Selin <sylvan.climb@gmail.com>
2018-07-06 12:13:44 +0300

    [bot.js] delete webHook in bot. Change setWebHook

--- bot.js
+++ bot.js
@@ -3,8 +3,10 @@ const db = require('./data/levelDb');
 const state = require('./data/state');
 
 const token = process.env.BOT_ACCESS_TOKEN;
+const appurl = process.env.APP_URL;
 
-const bot = new Bot(token, { polling: true });
+const bot = new Bot(token);
+bot.setWebHook(appurl + token);
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
